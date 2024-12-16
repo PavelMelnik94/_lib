4ec5ff171102b490f903e70b9effeffa242d0970
Pavel <p.melnik@intelico.su>
2024-12-16 12:07:32 +0300

    adjust package and vite config, up to 1.0.10

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,20 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
+
 export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            name: 'index',
-            fileName: (format) => `index.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['jsdom'],
-            output: {
-                globals: {
-                    jsdom: 'jsdom',
-                },
-            },
-        },
-    },
-    plugins: [
-			dts({
-					insertTypesEntry: true,
-					rollupTypes: true,
-					bundledPackages: ['dom-utils-minimize'],
-					outDir: ['dist/types'], // Явно указываем директорию для типов
-					compilerOptions: {
-							skipLibCheck: true,
-							declarationDir: './dist/types'
-					}
-			})
-	]
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: (format) => `index.${format}.js`,
+    }
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+      outDir: ['dist/types']
+    })
+  ]
 });
